407d732abd84c11d79b0f82274c9b6586d7791d0
Paul <paulyfwang@gmail.com>
2022-10-20 15:40:27 -0400

    Seems like there was a memleak, manually calling stop hook on close now

--- app.js
+++ app.js
@@ -3,27 +3,26 @@ const app = express();
 const rtsp = require('rtsp-ffmpeg')
 const boundary = '--5a22d388de01'
 
-var uri = `rtsp://${process.env.TAPO_USER}:${process.env.TAPO_PW}@webcam.c100/stream1`
-var stream = new rtsp.FFMpeg({
-    input: uri,
-    rate: 15,
-    quality: 3,
-})
+var uri = `rtsp://${process.env.TAPO_USER}:${process.env.TAPO_PW}@webcam.c100/stream2`
 
 app.get("/", (req, res) => {
+    var stream = new rtsp.FFMpeg({
+        input: uri,
+    })
+    
+    var stream_transform = (data) => {
+        res.write(`${boundary}\nContent-type: image/jpeg\nContent-length: ${data.length}\n\n`) 
+        res.write(data)
+        res.write(`\n`)
+    }
     res.status(200)
     res.setHeader('Content-Type', `multipart/x-mixed-replace; boundary=${boundary}`)
-    res.setHeader('Connection', 'close')
     res.setHeader('Pragma', 'no-cache')
-    stream.on('data',(data) => {
-        res.write(`${boundary}\nContent-type: image/jpg\nContent-length: ${data.length}\n\n`) 
-        res.write(data)
-    })
+    stream.on('data', stream_transform)
     req.on("close", () => {
+        stream.removeListener('data', stream_transform)
+        stream.stop()
         console.log("Client quit connection")
-    })
-    req.on("end", () => {
-        console.log("Client closed connection")
     })
 });
 
